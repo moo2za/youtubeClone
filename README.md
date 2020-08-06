eaef9254ee7fc4407842a44e9dedf1ed904a7962
moo2za <62229594+moo2za@users.noreply.github.com>
2020-08-06 16:07:01 +0900

    2020/08/06 16:00
    2020/08/06 16:00

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # youtubeClone
+
+Youtube Clone with Vanilla and Node JS
+
+## Pages:
+
+- [ ] Home
+- [x] Join
+- [x] Login
+- [x] Search
+- [ ] User Detail
+- [ ] Edit Profile
+- [ ] Change Password
+- [ ] Upload
+- [ ] Video Detail
+- [ ] Edit Video
---
+++ routes.js
@@ -0,0 +1,66 @@
+const { editVideo } = require("./controllers/videoController");
+
+//Global
+const HOME = "/";
+const JOIN = "/join";
+const LOGIN = "/login";
+const LOGOUT = "/logout";
+const SEARCH = "/search";
+
+//Users
+
+const USERS = "/users";
+const USER_DETAIL = "/:id";
+const EDIT_PROFILE = "/edit-profile";
+const CHANGE_PASSWORD = "/change-password";
+
+// Videos
+
+const VIDEOS = "/videos";
+const VIDEO_DETAIL = "/:id";
+const DELETE_VIDEO = "/:id/delete";
+const EDIT_VIDEO = "/:id/edit";
+const UPLOAD = "/upload";
+
+const routes = {
+  home: HOME,
+  join: JOIN,
+  login: LOGIN,
+  logout: LOGOUT,
+  search: SEARCH,
+  users: USERS,
+  userDetail: (id) => {
+    if (id) {
+      return `/users/${id}`;
+    } else {
+      return USER_DETAIL;
+    }
+  },
+  editProfile: EDIT_PROFILE,
+  changePassword: CHANGE_PASSWORD,
+  videos: VIDEOS,
+  upload: UPLOAD,
+  videoDetail: (id) => {
+    if (id) {
+      return `/videos/${id}`;
+    } else {
+      return VIDEO_DETAIL;
+    }
+  },
+  editVideo: (id) => {
+    if (id) {
+      return `/videos/${id}/edit`;
+    } else {
+      return EDIT_VIDEO;
+    }
+  },
+  deleteVideo: (id) => {
+    if (id) {
+      return `/videos/${id}/delete`;
+    } else {
+      return DELETE_VIDEO;
+    }
+  },
+};
+
+export default routes;
